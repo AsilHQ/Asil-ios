46cf67a6d4edbee78253dff2412949ba4ecd333a
Cem Sertkaya <cemsertkaya99@gmail.com>
2023-03-23 17:54:33 +0300

    Code refactoring: Youtube Filtration + YoutubeFiltrationWebRepository is added.

--- Sources/BraveWallet/YoutubeFiltration/YoutubeFiltrationManager.swift
+++ Sources/BraveWallet/YoutubeFiltration/YoutubeFiltrationManager.swift
@@ -10,13 +10,13 @@ import UIKit
 import WebKit
 import BraveShared
 
-
 public class YoutubeFiltrationManager {
     public static var shared = YoutubeFiltrationManager()
+    private let webRepository = YoutubeFiltrationWebRepository()
     
     private func getEmail(erik: Erik) {
         guard let filePath = Bundle.main.path(forResource: "email", ofType: "js") else {
-            print("Failed to find script.js file")
+            print("Youtube Filtration: Failed to find script.js file")
             return
         }
         
@@ -31,7 +31,7 @@ public class YoutubeFiltrationManager {
                         print("\(error)")
                     }
                 } else {
-                    print("successfull")
+                    print("Youtube Filtration: Script worked successfully")
                 }
             }
         } catch {
@@ -42,8 +42,7 @@ public class YoutubeFiltrationManager {
     public func getUserInformationsFromYoutube(view: UIView, webView: WKWebView) {
         
         if let url = webView.url?.absoluteString, url.contains("youtube.com") {
-            // User is on a YouTube page
-            print("User is on a YouTube page")
+            print("Youtube Filtration: User is on a YouTube page")
             let hiddenView = WKWebView(frame: view.bounds)
             hiddenView.isHidden = true
             view.addSubview(hiddenView)
@@ -52,17 +51,16 @@ public class YoutubeFiltrationManager {
                 self.getEmail(erik: erik)
             }
         } else {
-            // User is not on a YouTube page
-            print("User is not on a YouTube page")
+            print("Youtube Filtration: User is not on a YouTube page")
         }
     }
     
     func saveYoutubeInformations(dict: [String: Any]) {
         if let email = dict["email"] as? String, let name = dict["name"] as? String, let imgSrc = dict["imgSrc"] as? String {
-            if email != Preferences.YoutubeFiltration.email.value || Preferences.YoutubeFiltration.token.value == nil || Preferences.YoutubeFiltration.token.value == ""  {
+            if email != Preferences.YoutubeFiltration.email.value || Preferences.YoutubeFiltration.token.value == nil || Preferences.YoutubeFiltration.token.value == "" {
                 print("Youtube Filtration: Successfully signed-in")
-                authSession(email: email, name: name) { dict, error in
-                    if let dict = dict, let token = dict["token"]  {
+                webRepository.authSession(email: email, name: name) { dict, error in
+                    if let dict = dict, let token = dict["token"] {
                         Preferences.YoutubeFiltration.email.value = email
                         Preferences.YoutubeFiltration.username.value = name
                         Preferences.YoutubeFiltration.imageURL.value = imgSrc
@@ -77,109 +75,5 @@ public class YoutubeFiltrationManager {
         } else {
             print("Youtube Filtration: Anonymous user")
         }
-    }
-    
-    func authSession(email: String, name: String, completion: @escaping ([String: String]?, Error?) -> Void) {
-        register(email: email, name: name) { dict, error in
-            if error != nil {
-                self.login(email: email) { dict, error in
-                    if let dict = dict {
-                        completion(dict, nil)
-                    } else {
-                        completion(nil, nil)
-                    }
-                }
-            } else if let dict = dict {
-                completion(dict, nil)
-            } else { // The email has already been taken.
-                self.login(email: email) { dict, error in
-                    if let dict = dict {
-                        completion(dict, nil)
-                    } else {
-                        completion(nil, nil)
-                    }
-                }
-            }
-        }
-    }
-    
-    func login(email: String, name: String? = nil, completion: @escaping ([String: String]?, Error?) -> Void) {
-        let parameters = ["email": email]
-        let url = URL(string: "https://api.kahf.ai/api/v1/login")!
-        var request = URLRequest(url: url)
-        request.httpMethod = "POST"
-        request.setValue("application/json", forHTTPHeaderField: "Accept")
-        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
-        request.httpBody = try? JSONSerialization.data(withJSONObject: parameters, options: [])
-        let task = URLSession.shared.dataTask(with: request) { data, response, error in
-            guard let data = data, error == nil else {
-                completion(nil, error)
-                return
-            }
-            do {
-                if let dataDict = try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any] {
-                    var responseDict = [String: String]()
-                    if let token = dataDict["token"] as? String {
-                        responseDict["token"] = token
-                        if let data = dataDict["data"] as? [String: Any] {
-                            if let name = data["name"] as? String {
-                                responseDict["name"] = name
-                            }
-                            if let email = data["email"] as? String {
-                                responseDict["email"] = email
-                            }
-                        }
-                        completion(responseDict, nil)
-                    } else {
-                        completion(nil, nil)
-                    }
-                } else {
-                    completion(nil, nil)
-                }
-            } catch {
-                completion(nil, error)
-            }
-        }
-        task.resume()
-    }
-    
-    func register(email: String, name: String? = nil, completion: @escaping ([String: String]?, Error?) -> Void) {
-        let parameters = ["email": email, "name": name]
-        let url = URL(string: "https://api.kahf.ai/api/v1/register")!
-        var request = URLRequest(url: url)
-        request.httpMethod = "POST"
-        request.setValue("application/json", forHTTPHeaderField: "Accept")
-        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
-        request.httpBody = try? JSONSerialization.data(withJSONObject: parameters, options: [])
-        let task = URLSession.shared.dataTask(with: request) { data, response, error in
-            guard let data = data, error == nil else {
-                completion(nil, error)
-                return
-            }
-            do {
-                if let dataDict = try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any] {
-                    var responseDict = [String: String]()
-                    if let token = dataDict["token"] as? String {
-                        responseDict["token"] = token
-                        if let data = dataDict["data"] as? [String: Any] {
-                            if let name = data["name"] as? String {
-                                responseDict["name"] = name
-                            }
-                            if let email = data["email"] as? String {
-                                responseDict["email"] = email
-                            }
-                        }
-                        completion(responseDict, nil)
-                    } else {
-                        completion(nil, nil)
-                    }
-                } else {
-                    completion(nil, nil)
-                }
-            } catch {
-                completion(nil, error)
-            }
-        }
-        task.resume()
     }
 }
